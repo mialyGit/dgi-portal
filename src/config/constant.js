d7f1317dc6852fe61cc6f4f4877124e97b08bd83
AVOTRINIAINA <avotriniainamialison1106@gmail.com>
2022-10-12 14:31:42 +0300

    First api call

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,4 +1,4 @@
-export const API_SERVER = "http://localhost:8000/";
-
+//export const API_SERVER = "http://localhost:8000/";
+export const API_SERVER = "https://api-dgi-portal.herokuapp.com/";
 export const basename = "/dgi-portal";
 
